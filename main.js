a8c5d16e2da61b3119e6d1970a280421a6bf43d2
Aryaveer-Coding <79976916+Aryaveer-Coding@users.noreply.github.com>
2022-02-17 16:44:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
+song1 = "";
+song2 = "";
+
+function preload()
+{
+    song1 = loadSong("music.mp3")
+    song2 = loadSong("music2.mp3")
+}
+
 function setup()
 {
     canvas = createCanvas(550, 500);
     canvas.position(500, 160);
+}
+
+function setup()
+{
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw()
+{
+    image(video, 0, 0, 600, 500);
 }
